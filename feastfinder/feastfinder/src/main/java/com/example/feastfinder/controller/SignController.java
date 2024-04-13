d8b888b336ee37b5d911f323ec47691c80781ae3
Nishok777 <125253923+Nishok777@users.noreply.github.com>
2024-04-13 09:57:05 +0530

    Add files via upload

---
+++ feastfinder/feastfinder/src/main/java/com/example/feastfinder/controller/SignController.java
@@ -0,0 +1,39 @@
+package com.example.feastfinder.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.feastfinder.model.Signup;
+import com.example.feastfinder.service.SignService;
+
+@RestController
+@RequestMapping("/sign")
+public class SignController {
+
+    @Autowired
+    private SignService signService;
+
+    @PostMapping("/signup")
+    public String signUp(@RequestBody Signup signup) {
+        Signup sign = signService.getUser(signup.getEmailId());
+        if (sign == null) {
+            signService.signup(signup);
+            return "Signed up successfully";
+        }
+        return "EmailId already exists, please use another EmailId";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam String emailId, @RequestParam String password) {
+        Signup log = signService.loginCheck(emailId, password);
+        if (log == null) {
+            return "Invalid Email Id or password";
+        }
+        return "Login Successful";
+    }
+}
---
+++ feastfinder/feastfinder/src/main/java/com/example/feastfinder/repository/MenuRepo.java
@@ -0,0 +1,12 @@
+package com.example.feastfinder.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.feastfinder.model.Menu;
+
+@Repository
+public interface MenuRepo extends JpaRepository<Menu,Integer>{
+    Menu findByFoodName(String foodName);
+    
+}
